2ef806e1b0ad452e671e825e553466f766181760
Mahesh <chaitanyadarlapudi@gmail.com>
2025-08-21 16:34:10 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CounterComponent } from './counter/counter.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    CounterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
